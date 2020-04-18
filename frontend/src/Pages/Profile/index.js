24417721f05b17d76c6a22241612d2e6208fa06d
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2020-04-17 23:46:20 -0300

    Profile atualiza os pedidos em tempo real

--- frontend/src/Pages/Profile/index.js
+++ frontend/src/Pages/Profile/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
+import React, { useState, useEffec, useEffect} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import swal from 'sweetalert';
+import api from '../../services/api';
 import './style.css';
 
 export default function Profile(){
 
+    const [pedidos, setPedidos] = useState([]);
+    const [refresh, setRefresh] = useState(true);
+    const history = useHistory();
     const token = localStorage.getItem('token');
     const uid = localStorage.getItem('uid');
     const name = localStorage.getItem('name');
+
+    useEffect(() => {
+        api.get('pedido')
+        .then(
+            response => {
+                setPedidos(response.data)
+            })
+    }, [refresh]);
+
+    async function handleDeletePedido(pedidoid){
+        try{
+            await api.delete('pedido',
+                { 
+                    data : {
+                        uid,
+                        pedidoid
+                    },
+                    headers: {
+                        Authorization : token
+                    }
+                }
+            );
+            setPedidos(pedidos.filter(pedido => pedido.uid !== pedidoid));
+        }catch(err){
+            swal("Erro ao deletar pedido", "error")
+        }
+    }
+
+    function handleLogout(){
+        localStorage.clear()
+        history.push('/');
+    }
     
+    setTimeout(() => setRefresh(!refresh), 150)
     return (
         <div className="profile-container">
             <header>
@@ -18,7 +56,7 @@ export default function Profile(){
                     Cadastar novo Cardápio
                 </Link>
 
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} style={{color: "#E02041"}}/>
                 </button>
             </header>
@@ -26,39 +64,20 @@ export default function Profile(){
         <h1>Pedidos</h1>
 
         <ul>
-            <li>
-                <strong>Pedido</strong>
-                <p>Arroz  x2</p>
-                <p>Feijão x1</p>
-                <p>Frango x1</p>
-                <p>batata x1</p>
-                <strong>Endereço</strong>
-                <p>Rua Radialista Coelho Alves - 635</p>
-                <strong>Valor</strong>
-                <p>R$ 50 reais</p>
-            </li>
-            <li>
-                <strong>Pedido</strong>
-                <p>Arroz  x2</p>
-                <p>Feijão x1</p>
-                <p>Frango x1</p>
-                <p>batata x1</p>
-                <strong>Endereço</strong>
-                <p>Rua Radialista Coelho Alves - 635</p>
-                <strong>Valor</strong>
-                <p>R$ 50 reais</p>
-            </li>
-            <li>
-                <strong>Pedido</strong>
-                <p>Arroz  x2</p>
-                <p>Feijão x1</p>
-                <p>Frango x1</p>
-                <p>batata x1</p>
-                <strong>Endereço</strong>
-                <p>Rua Radialista Coelho Alves - 635</p>
-                <strong>Valor</strong>
-                <p>R$ 50 reais</p>
-            </li>
+            {pedidos.map(pedido => 
+                <li>
+                    <strong>Pedido {pedido.uid}</strong>
+                        
+                    <strong>Endereço</strong>
+                        <p>{pedido.address}</p>
+                    <strong>Valor</strong>
+                        <p>{Intl.NumberFormat('pt-br', { style: 'currency', currency: "BRL"}).format(pedido.price)}</p>
+                    <button onClick={() => handleDeletePedido(pedido.uid)} type="button">
+                        <FiTrash2 size={20} color="#a8a8b3"/>
+                    </button>
+                </li>
+            )}
+            
         </ul>
 
         <h1>Cardapio</h1>
@@ -66,11 +85,12 @@ export default function Profile(){
         <ul>
             <li>
                 <strong>Nome</strong>
-                <p>Frango</p>
+                <p>Arroz</p>
                 <strong>Descrição</strong>
-                <p>Frango Comum</p>
+                <p>Arroz Comum</p>
                 <strong>Preço</strong>
-                <p>R$ 5 reais</p>
+                <p>R$ 10 reais</p>
+                <img src="http://localhost:3333/images/144ec0df.png"/>
             </li>
         </ul>
     </div> 
