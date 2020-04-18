457127bb83a090c69a31642f042c044c617dee14
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2020-04-17 23:45:29 -0300

    Adicionado PrivateRoute para aumentar a segurança

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Logon from './Pages/Logon';
 import Profile from './Pages/Profile';
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        (localStorage.getItem('token') !== null) ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+        )
+      }
+   />
+);
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Logon}/>
-                <Route path="/profile" component={Profile}/>
+                <PrivateRoute path="/profile" component={Profile}/>
+                <Route path="*" component={() => <h1>Page not found</h1>} />
             </Switch>
         </BrowserRouter>
     );
