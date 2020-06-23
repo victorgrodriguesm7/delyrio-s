315d4da9c931e45995815e48121d82803e76aea0
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2020-06-23 16:24:36 -0300

    Aplicativo é forçado a executar em modo retrato

--- aplicativo/lib/src/app.dart
+++ aplicativo/lib/src/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'Pages/Login/loginPage.dart';
 
 class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'De-Lyrios',
       debugShowCheckedModeBanner: false,
