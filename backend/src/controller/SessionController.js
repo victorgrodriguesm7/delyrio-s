d5b124539684704153ae458281e0d1f52840d244
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2020-06-23 14:33:05 -0300

    Correção de Erros no login, Login baseado no email

--- backend/src/controller/SessionController.js
+++ backend/src/controller/SessionController.js
@@ -34,34 +34,33 @@ module.exports = {
         }
     },
     async UserLogin(request,response){
-        const { phonenumber, password} = request.body
-
+        const {email, password} = request.body
+        
         try{
             const {uid, ...cryptPass} = await connection.from("user")
                 .where({
-                    phonenumber
+                    "email": email
                 }).select(["uid", "password"])
                 .first();
+                bcrypt.compare(password, cryptPass["password"], async function(error, result){
+                    if (result){
+                        const token = crypto.randomBytes(8).toString('HEX')
+                        await connection('user').where({
+                            uid,
+                        }).update({
+                            token
+                        });
+        
+                        return response.status(200).json({
+                            token,
+                            uid
+                        });
+                    }else{
+                        return response.status(404).json({ error: "Incorrect Password" })
+                    }
+                });
         }catch(err){
             return response.status(404).json({error: "User not found"});
-        }
-        bcrypt.compare(password, cryptPass["password"], async function(error, result){
-            if (result){
-
-                const token = crypto.randomBytes(8).toString('HEX')
-                await connection('user').where({
-                    uid,
-                }).update({
-                    token
-                });
-
-                return response.status(200).json({
-                    token,
-                    uid
-                });
-            }else{
-                return response.status(404).json({ error: "Incorrect Password" })
-            }
-        });
+        } 
     }
 }
