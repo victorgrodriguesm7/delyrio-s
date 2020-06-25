0bb38cdd88bc520b451d3f16196fafd3d3a56075
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2020-06-25 20:00:40 -0300

    Pagina de Login finalizada

--- aplicativo/lib/src/Pages/Login/loginPage.dart
+++ aplicativo/lib/src/Pages/Login/loginPage.dart
@@ -14,7 +14,7 @@ class _LoginPageState extends State<LoginPage> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
   LoginController controller;
   bool isLoading = false;
-
+  bool isObscure = true;
   @override
   void initState() {
     super.initState();
@@ -42,111 +42,143 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      key: _scaffoldKey,
-      body: SafeArea(
-      child: Center(
-          child: Stack(
-        children: [
-          Positioned(
-              left: -width * 0.32,
-              top: -height * 0.7,
-              child: Transform.rotate(
-                angle: 3.14159265359 / 8,
-                child: Container(
-                    height: height * 2, width: height * 0.5, color: Colors.red),
-              )),
-          Positioned(
-              top: height * 0.02,
-              left: width * 0.06,
-              child: Image.asset(
-                'assets/images/logo-borda.png',
-                width: 47,
-                height: 47,
-              )),
-          Positioned(
-              top: height * 0.02,
-              left: width * 0.06,
-              child: Image.asset(
-                'assets/images/logo.png',
-                width: 46,
-                height: 46,
-              )),
-          Positioned(
-              top: height * 0.08,
-              left: width * 0.13,
-              child: Text("De Lyrio's  ",
-                  style: TextStyle(
-                    fontSize: 50,
-                    color: Colors.orangeAccent.withOpacity(0.9),
-                    shadows: [
-                      Shadow(
-                        blurRadius: 4.5,
-                        color: Colors.grey,
-                        offset: Offset(3.5, 3.5),
-                      ),
-                    ],
-                  )
-                )
-              ),
-          Positioned(
-              child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Center(
-                      child: Form(
-                        key: controller.formKey,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                          TextFormField(
-                            validator: (value){
-                              if (value.isEmpty) {
-                                return 'Campo não pode ser vazio';
-                              } else if (!value.contains('@')) {
-                                return 'Email não é válido';
-                              }
-                              return null;
-                            },
-                            onChanged: controller.userEmail,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white38,
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                    color: Colors.redAccent, width: 3.0),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide:
-                                    BorderSide(color: Colors.grey, width: 3.0),
-                              ),
-                              hintText: 'Email',
-                            ),
+        return Scaffold(
+          key: _scaffoldKey,
+          body: SafeArea(
+          child: Center(
+              child: Stack(
+            children: [
+              Positioned(
+                  left: -width * 0.32,
+                  top: -height * 0.7,
+                  child: Transform.rotate(
+                    angle: 3.14159265359 / 8,
+                    child: Container(
+                        height: height * 2, width: height * 0.5, color: Colors.red),
+                  )),
+              Positioned(
+                  top: height * 0.02,
+                  left: width * 0.06,
+                  child: Image.asset(
+                    'assets/images/logo-borda.png',
+                    width: 47,
+                    height: 47,
+                  )),
+              Positioned(
+                  top: height * 0.02,
+                  left: width * 0.06,
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 46,
+                    height: 46,
+                  )),
+              Positioned(
+                  top: height * 0.08,
+                  left: width * 0.13,
+                  child: Text("De Lyrio's  ",
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.orangeAccent.withOpacity(0.9),
+                        shadows: [
+                          Shadow(
+                            blurRadius: 4.5,
+                            color: Colors.grey,
+                            offset: Offset(3.5, 3.5),
                           ),
-                          TextField(
-                            obscureText: true,
-                            onChanged: controller.userPassword,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.white38,
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                    color: Colors.redAccent, width: 3.0),
+                        ],
+                      )
+                    )
+                  ),
+              Padding(padding: EdgeInsets.only(top: 16)),
+              Positioned(
+                  child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Center(
+                          child: Form(
+                            key: controller.formKey,
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                              TextFormField(
+                                validator: (value){
+                                  if (value.isEmpty) {
+                                    return 'Campo não pode ser vazio';
+                                  } else if (!value.contains('@')) {
+                                    return 'Email não é válido';
+                                  }
+                                  return null;
+                                },
+                                onChanged: controller.userEmail,
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white38,
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                        color: Colors.redAccent, width: 3.0),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide:
+                                        BorderSide(color: Colors.grey, width: 3.0),
+                                  ),
+                                  hintText: 'Email',
+                                ),
                               ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide:
-                                    BorderSide(color: Colors.grey, width: 3.0),
-                              ),
-                              hintText: 'Senha',
-                            ),
-                          ),
+                               Stack(children: <Widget>[
+                                  Positioned(
+                                      child: TextFormField(
+                                    onChanged: controller.userPassword,
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return 'Campo não pode ser vazio';
+                                      }
+                                      return null;
+                                    },
+                                    obscureText: isObscure,
+                                keyboardType: TextInputType.multiline,
+                                decoration: InputDecoration(
+                                   errorStyle: TextStyle(color:Colors.black87,
+                                  fontSize: 15),
+                                  filled: true,
+                                  fillColor: Colors.white38,
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                        color: Colors.redAccent, width: 3.0),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                        color: Colors.grey, width: 3.0),
+                                  ),
+                                  hintText: 'Sua senha',
+                                ),
+                              )),
+                              Positioned(
+                                right: -width * 0.01,
+                                bottom: width * 0.01,
+                                child: FlatButton(
+                                  child: isObscure
+                                      ? Icon(Icons.visibility_off)
+                                      : Icon(Icons.visibility),
+                                  onPressed: () {
+                                    setState(() {
+                                      isObscure = !isObscure;
+                                    });
+                                  },
+                                ),
+                              )
+                            ]),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
                               RaisedButton(
+                                shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side: BorderSide(color: Colors.yellow)
+                                 ),
+                                color:Colors.orangeAccent ,
                                   onPressed: isLoading
                                     ? null
                                     : () async {
@@ -173,6 +205,11 @@ class _LoginPageState extends State<LoginPage> {
                                       MaterialPageRoute(builder: (_) => CadastroPage()),
                                     );
                                   },
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(color: Colors.yellow)
+                                  ),
+                                color:Colors.orangeAccent ,
                                   child: Text("Cadastrar"),
                               )
                             ],
